af0280198f1c940b4b8e656d1a588aaeaa310964
lioarce01 <lioarce1@gmail.com>
2024-10-23 14:19:50 -0300

    push comm positional added

--- commands/push-pull-commands/push.js
+++ commands/push-pull-commands/push.js
@@ -5,10 +5,26 @@ const git = simpleGit();
 export default {
   command: "push",
   describe: "Push changes to a remote repository",
-  builder: () => {},
-  handler: async () => {
+  builder: () => {
+    yargs.positional("origin", {
+      describe: "repository to push to",
+      type: "string",
+      demandOption: true,
+    });
+    yargs.positional("branch", {
+      describe: "branch to push",
+      type: "string",
+      demandOption: true,
+    });
+  },
+  handler: async (argv) => {
+    const { origin, branch } = argv;
+
     try {
-      await git.push("origin", "main");
+      if (!origin || !branch) {
+        console.error("Error: origin and branch are required");
+      }
+      await git.push(origin, branch);
       console.log("Changes pushed to a remote repository");
     } catch (error) {
       console.error("Error pushing changes:", error.message);
